603e9aac577e1eb3a01c88d72004b4ac6b05d627
Sogand <sogandmohamadipour7@gmail.com>
2025-11-22 22:55:34 +0330

    adding reactRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/product";
+import Pricing from "./pages/pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
   return (
-    <div>
-      WorldWise
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="Product" element={<Product />} />
+          <Route path="Pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,7 @@
+export default function PageNotFount() {
+  return (
+    <div>
+      Page not Found!
+    </div>
+  )
+}
