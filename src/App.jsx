8f36a93bc587c30aa77c038dae3423350f3ffd31
Sogand <sogandmohamadipour7@gmail.com>
2025-11-23 21:45:20 +0330

    fixing vercel issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Product from "./pages/product";
-import Pricing from "./pages/pricing";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
