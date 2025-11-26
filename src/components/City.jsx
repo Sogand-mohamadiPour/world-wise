54f1b5dbbad3b87274d4f4c652e510074759e2da
Sogand <sogandmohamadipour7@gmail.com>
2025-11-26 22:51:55 +0330

    data passed by params

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,7 @@
 // import styles from "./City.module.css";
 
+import { useParams } from "react-router-dom";
+
 // const formatDate = (date) =>
 //   new Intl.DateTimeFormat("en", {
 //     day: "numeric",
@@ -9,6 +11,8 @@
 //   }).format(new Date(date));
 
 function City() {
+  const { id } = useParams();
+
   // TEMP DATA
   // const currentCity = {
   //   cityName: "Lisbon",
@@ -19,7 +23,7 @@ function City() {
 
   // const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City</h1>
+  return <h1>{id}</h1>;
 
   // return (
   //   <div className={styles.city}>
