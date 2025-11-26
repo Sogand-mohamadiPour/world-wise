ca2da1902d065a2ce5a159390c17a9bdf5f4bd8c
Sogand <sogandmohamadipour7@gmail.com>
2025-11-26 23:34:12 +0330

    data delivered by query string

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,15 @@
+import { useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 export default function Map() {
+  const [searchParams , setSearchParams]  = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
   return (
     <div className={styles.mapContainer}>
-      Map
+      <h1>lat: {lat}</h1>
+      <h1>lng: {lng}</h1>
     </div>
   )
 }
