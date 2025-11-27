dc4d8817db73e7ddd5a85ea21841df12d2549dd8
Sogand <sogandmohamadipour7@gmail.com>
2025-11-27 23:04:29 +0330

    programmatic navigator started

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,18 @@
-import { useSearchParams } from 'react-router-dom'
-import styles from './Map.module.css'
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
 
 export default function Map() {
-  const [searchParams , setSearchParams]  = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+ const navigate = useNavigate();
+
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>lat: {lat}</h1>
       <h1>lng: {lng}</h1>
     </div>
-  )
+  );
 }
