1b5093e5dd4a079b332f7f9044c8e124b513e854
Sogand <sogandmohamadipour7@gmail.com>
2025-12-10 00:07:17 +0330

    2 parts of auth done

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,16 +1,23 @@
 import styles from "./User.module.css";
 import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 
 function User() {
   const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
   if (!user) return null;
+
+  function handleClick() {
+    logout();
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
       <img src={user.avatar} alt={user.name} />
       <span>Welcome, {user.name}</span>
-      <button onClick={logout}>Logout</button>
+      <button onClick={handleClick}>Logout</button>
     </div>
   );
 }
--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -30,7 +30,7 @@ const FAKE_USER = {
   name: "Jack",
   email: "jack@example.com",
   password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
+  avatar: "https://i.pravatar.cc/102?u=zz",
 };
 
 function AuthProvider({ children }) {
