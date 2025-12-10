393ccf1142231b3f72674c1003999ee73eced0a4
Sogand <sogandmohamadipour7@gmail.com>
2025-12-10 23:24:17 +0330

    protected routes added

---
+++ src/pages/ProtectedRoutes.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect } from "react";
+
+function ProtectedRoutes({ children }) {
+  const { isAthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!isAthenticated) navigate("/");
+    },
+    [isAthenticated, navigate]
+  );
+  return children;
+}
+
+export default ProtectedRoutes;
